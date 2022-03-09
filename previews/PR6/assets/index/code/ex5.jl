7340529c5d5789dc8d8c53788adfc7c719ed72f3
abelsiqueira <abelsiqueira@users.noreply.github.com>
2022-03-09 00:14:16 +0000

    Deploying to gh-preview from  @ e1b0007363a45797c232d44f29bcab8105d5f311 🚀

---
+++ previews/PR6/assets/index/code/ex5.jl
@@ -0,0 +1,6 @@
+# This file was generated, do not modify it. # hide
+nlp = GridapPDENLPModel(x0, f, trian, Ypde, Ycon, Xpde, Xcon, op, name = "Control elastic membrane")
+
+using NLPModels
+
+(get_nvar(nlp), get_ncon(nlp))
---
+++ previews/PR6/assets/index/code/ex9.jl
@@ -0,0 +1,2 @@
+# This file was generated, do not modify it. # hide
+stats_ipopt = ipopt(nlp, x0 = stats_trunk.solution, tol = 1e-5, print_level = 0)
