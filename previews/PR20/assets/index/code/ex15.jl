278261b010124019641a07e9d358f7eb3b11f272
tmigot <tmigot@users.noreply.github.com>
2023-01-30 18:26:30 +0000

    Deploying to gh-preview from  @ e044342dc0fea886f0ca029f33abacd1b9dfd75e 🚀

---
+++ previews/PR20/assets/index/code/ex15.jl
@@ -0,0 +1,3 @@
+# This file was generated, do not modify it. # hide
+(stats_ipopt.objective, stats_ipopt.primal_feas, stats_ipopt.dual_feas),
+(stats_dci.objective, stats_dci.primal_feas, stats_dci.dual_feas)
---
+++ previews/PR20/assets/index/code/ex4.jl
@@ -0,0 +1,21 @@
+# This file was generated, do not modify it. # hide
+trian = Triangulation(model)
+degree = 1
+dΩ = Measure(trian, degree)
+
+yd(x) = -x[1]^2
+α = 1e-2
+function f(y, u)
+  ∫(0.5 * 1e2 * (yd - y) * (yd - y) + 0.5 * α * u * u) * dΩ
+end
+
+ω = π - 1 / 8
+h(x) = -sin(ω * x[1]) * sin(ω * x[2])
+function res(y, u, v)
+  ∫(∇(v) ⊙ ∇(y) - v * u - v * h) * dΩ
+end
+op = FEOperator(res, Y, Xpde)
+
+npde = Gridap.FESpaces.num_free_dofs(Ypde)
+ncon = Gridap.FESpaces.num_free_dofs(Ycon)
+x0 = zeros(npde + ncon);
